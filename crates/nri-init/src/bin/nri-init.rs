e9f3793327b93c91b7659f97929cd5480b35e469
Jonathan Perry <yonch@yonch.com>
2025-09-08 09:25:08 -0500

    nri-init: log 'nri-init done' on success; CI: wait for this log line in DS tests and avoid echo collision

--- crates/nri-init/src/bin/nri-init.rs
+++ crates/nri-init/src/bin/nri-init.rs
@@ -21,6 +21,7 @@ fn main() {
     match nri_init::run(opts) {
         Ok(out) => {
             tracing::info!("Completed: configured={}, restarted={}, socket={}", out.configured, out.restarted, out.socket_available);
+            tracing::info!("nri-init done");
             std::process::exit(0);
         }
         Err(e) => {
@@ -29,4 +30,3 @@ fn main() {
         }
     }
 }
-
